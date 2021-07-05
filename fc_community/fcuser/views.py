c75f36819744177ea2bb42fea9b91538422ba727
mang0206 <alswosp0206@naver.com>
2021-07-05 14:07:34 +0900

    ;

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -47,13 +47,7 @@ def login(request):
 
 
 def home(request):
-    user_id = request.session.get('user')
-
-    if user_id:
-        fcuser = Fcuser.objects.get(pk=user_id)
-        return HttpResponse(fcuser.username)
-
-    return HttpResponse('Home!')
+    return render(request, 'home.html')
 
 
 def logout(request):
